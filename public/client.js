07cc8ff192121466e18ccb9770b593768346b77b
Sabrina Markon <sabrina@phpsitescripts.com>
2016-12-28 18:38:48 -0700

    HTML output div added in addition to logging to console. Input field, output div, and command array is reset, clearing commands that have already run, before running a new command chain.

--- public/client.js
+++ public/client.js
@@ -13,9 +13,17 @@ $(function() {
     });
   });
 
+clearCommands();
+
 });
 
 var commands = [];
+
+function clearCommands() {
+  document.getElementById('showchain').innerHTML = '';
+  document.getElementById('output').innerHTML = '';
+  commands = [];
+}
 
 function addCommand(command) {
   //alert(command);
@@ -29,6 +37,8 @@ function addCommand(command) {
 
 function chainCommands(commands) {
   
+    clearCommands();
+    
     for ( i = 0; i < commands.length; i++ ) {
     //console.log(commands[i]);
     // get the function name for the command:
@@ -41,27 +51,38 @@ function chainCommands(commands) {
 
 function wakeUp() {
   console.log('Woke up');
-
+  var output = document.getElementById('output');
+  output.innerHTML = output.innerHTML + 'Woke up<br>';
 }
 
 function findTrash() {
   console.log('Found trash');
+  var output = document.getElementById('output');
+  output.innerHTML = output.innerHTML + 'Found trash<br>';
 }
 
 function pickupTrash() {
   console.log('Picked up trash');
+  var output = document.getElementById('output');
+  output.innerHTML = output.innerHTML + 'Picked up trash<br>';
 }
 
 function changeBabyDiapers() {
   console.log('Changed baby\'s diapers');
+  var output = document.getElementById('output');
+  output.innerHTML = output.innerHTML + 'Changed baby\'s diapers<br>';
 }
 
 function makeDinner() {
   console.log('Made dinner');
+  var output = document.getElementById('output');
+  output.innerHTML = output.innerHTML + 'Made dinner<br>';  
 }
 
 function shutDown() {
   console.log('Shut down for the day');
+  var output = document.getElementById('output');
+  output.innerHTML = output.innerHTML + 'Shut down for the day<br>';  
 }
 
 
